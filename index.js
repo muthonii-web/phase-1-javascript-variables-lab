acca3d9749042c1863960ea6f55d51ad4db81e9a
pauline muthoni <kinyanjuipauline69@gmail.com>
2025-03-06 12:02:47 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Code your solution in this file!
-let companyName = 'Scuber'
-const companyName = 'Scuber'
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
